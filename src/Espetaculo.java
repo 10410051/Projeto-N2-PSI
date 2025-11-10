43fe1ff15c5ca13f756b66b3fb148408d2bc149a
10410051 <226614468+10410051@users.noreply.github.com>
2025-11-10 23:53:00 +0000

    Preparaçao do Menu => MackTheather

--- src/Espetaculo.java
+++ src/Espetaculo.java
@@ -33,4 +33,5 @@ public class Espetaculo {
         return super.toString();
     }
 
+    
 }
---
+++ src/MackTheather.java
@@ -0,0 +1,35 @@
+public class App {
+    public static void main(String[] args) {
+
+        System.out.println("\t\t\tMENU\t\t\t");
+        System.out.println("1. Cadastrar Espetaculo");
+        System.out.println("2. Cadastrar Cliente");
+        System.out.println("3. Compra de Entradas");
+        System.out.println("4. Sair");
+        System.out.println("Selecione uma opção:");
+
+        int opcao;
+        do {
+            switch (opcao) {
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+
+                default:
+                    System.out.println("Opção Invalida, tente novamente.");
+                    break;
+            }
+        } while (opcao != 0);
+
+        
+    }
+}
