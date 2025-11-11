f03729c94221fe331f17e403b5d6629a72f210c0
mtdevofficial <142057421+mtdevofficial@users.noreply.github.com>
2025-11-11 18:10:56 -0300

    [A] => EntradaProfessor

--- src/EntradaProfessor.java
+++ src/EntradaProfessor.java
@@ -1,4 +1,10 @@
 public class EntradaProfessor extends Entrada {
+    public EntradaProfessor(Espetaculo espetaculo, int numeroDoAssento) {
+        super(espetaculo, numeroDoAssento);
+    }
 
-    
+    @Override
+    public double calculaValor() {
+        return espetaculo.getPreco() * 0.6; 
+    }
 }
