8cf70985bbdd86e227868170b1e55be389b6f61d
mtdevofficial <142057421+mtdevofficial@users.noreply.github.com>
2025-11-11 18:08:27 -0300

    [A] => Entrada

--- src/Entrada.java
+++ src/Entrada.java
@@ -1,3 +1,20 @@
-public class Entrada {
-    
+public abstract class Entrada {
+    protected int numeroDoAssento;
+    protected Espetaculo espetaculo;
+
+    public Entrada(Espetaculo espetaculo, int numeroDoAssento) {
+        this.espetaculo = espetaculo;
+        this.numeroDoAssento = numeroDoAssento;
+    }
+
+    public int getNumeroDoAssento() {
+        return numeroDoAssento;
+    }
+
+    public abstract double calculaValor();
+
+    @Override
+    public String toString() {
+        return String.format("Assento %02d - R$ %.2f", numeroDoAssento, calculaValor());
+    }
 }
