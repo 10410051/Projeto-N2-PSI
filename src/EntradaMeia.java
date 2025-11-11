eeacb6b006b3c1ec3bb938369a0a08c6cbbf1f4f
mtdevofficial <142057421+mtdevofficial@users.noreply.github.com>
2025-11-11 18:10:04 -0300

    [A] => EntradaMeia

--- src/EntradaMeia.java
+++ src/EntradaMeia.java
@@ -1,3 +1,10 @@
 public class EntradaMeia extends Entrada {
-    
+    public EntradaMeia(Espetaculo espetaculo, int numeroDoAssento) {
+        super(espetaculo, numeroDoAssento);
+    }
+
+    @Override
+    public double calculaValor() {
+        return espetaculo.getPreco() * 0.5;
+    }
 }
