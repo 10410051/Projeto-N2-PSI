386e764975134d13509d25a13b122223297e8d18
mtdevofficial <142057421+mtdevofficial@users.noreply.github.com>
2025-11-11 18:09:23 -0300

    [A] => EntradaInteira

--- src/EntradaInteira.java
+++ src/EntradaInteira.java
@@ -1,5 +1,10 @@
 public class EntradaInteira extends Entrada {
-    
-}
-    
+    public EntradaInteira(Espetaculo espetaculo, int numeroDoAssento) {
+        super(espetaculo, numeroDoAssento);
+    }
 
+    @Override
+    public double calculaValor() {
+        return espetaculo.getPreco();
+    }
+}
