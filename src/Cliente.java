d864b5b26f367cf7137106924f907a1a7455aae3
mtdevofficial <142057421+mtdevofficial@users.noreply.github.com>
2025-11-11 18:14:33 -0300

    [A] => Cliente

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,15 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cliente {
     private String nome;
     private String cpf;
+    private List<Pedido> pedidos;
 
     public Cliente(String nome, String cpf) {
         this.nome = nome;
         this.cpf = cpf;
+        this.pedidos = new ArrayList<>();
     }
 
-    public void adicionaPedido(Pedido pedido) {
-        
+    public String getNome() {
+        return nome;
     }
 
-    
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void adicionaPedido(Pedido p) {
+        pedidos.add(p);
+    }
+
+    public List<Pedido> getPedidos() {
+        return pedidos;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s - CPF: %s", nome, cpf);
+    }
 }
